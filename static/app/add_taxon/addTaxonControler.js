ab24ede8ffd19d4d210f3a1a32332c1e7dc65abe
GirardinClaire <claire-girardin@hotmail.fr>
2024-07-17 13:33:52 +0000

    paramétrage directive autocomplétion EN COURS

--- static/app/add_taxon/addTaxonControler.js
+++ static/app/add_taxon/addTaxonControler.js
@@ -4,31 +4,7 @@ app.controller('addTaxonCtrl', ['$scope', '$http', 'backendCfg',
         //--------------------- Valeurs par défaut ------------------------------------
         var self = this;
         self.route='addTaxon';
-        self.newTaxon = {};
-
-        // Exemples de listes de suggestions
-        self.regneSuggestions = ["Animalia", "Plantae", "Fungi"];
-        self.phylumSuggestions = ["Chordata", "Arthropoda", "Mollusca"];
-        self.classeSuggestions = ["Mammalia", "Aves", "Insecta"];
-        self.ordreSuggestions = ["Carnivora", "Passeriformes", "Coleoptera"];
-        self.familleSuggestions = ["Felidae", "Fringillidae", "Carabidae"];
-        self.sousFamilleSuggestions = ["Pantherinae", "Carduelinae", "Carabinae"];
-        self.tribuSuggestions = ["Pantherini", "Fringillini", "Carabini"];
         
-        // Autocomplétion pour chaque champ
-        self.getSuggestions = function(input, type) {
-            switch(type) {
-                case 'regne': return self.regneSuggestions;
-                case 'phylum': return self.phylumSuggestions;
-                case 'classe': return self.classeSuggestions;
-                case 'ordre': return self.ordreSuggestions;
-                case 'famille': return self.familleSuggestions;
-                case 'sousFamille': return self.sousFamilleSuggestions;
-                case 'tribu': return self.tribuSuggestions;
-            }
-        };
-        
-
         // Gestion des droits
         // self.userRights = loginSrv.getCurrentUserRights();
 
@@ -47,14 +23,6 @@ app.controller('addTaxonCtrl', ['$scope', '$http', 'backendCfg',
             // });
             console.log('Taxon ajouté avec succès');
         };
-
-
-            
-            
-            
-
-
-        
 
         // Autres fonctions nécessaires pour la gestion de cette page
 
