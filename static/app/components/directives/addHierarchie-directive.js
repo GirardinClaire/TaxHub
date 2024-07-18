4bb5a42555f0dfaaa4423fd60e0634016c3e4bd9
GirardinClaire <claire-girardin@hotmail.fr>
2024-07-18 13:56:00 +0000

    autocomplétions SFM et TR

--- static/app/components/directives/addHierarchie-directive.js
+++ static/app/components/directives/addHierarchie-directive.js
@@ -10,16 +10,33 @@ app.directive('addHierarchieDir', ['$http', 'backendCfg', function ($http,backen
       //Initialisation
       $scope.$watch('taxHierarchieSelected', function(newVal, oldVal) {
         if ($scope.taxHierarchieSelected) {
-          if ($scope.taxHierarchieSelected.regne) $scope.taxHierarchieSelectedKD = {'regne': $scope.taxHierarchieSelected.regne, 'nb_tx_kd': $scope.taxHierarchieSelected.nb_tx_kd };
+          if ($scope.taxHierarchieSelected.regne)
+            $scope.taxHierarchieSelectedKD = {'regne': $scope.taxHierarchieSelected.regne, 'nb_tx_kd': $scope.taxHierarchieSelected.nb_tx_kd };
           else $scope.taxHierarchieSelectedKD = undefined;
-          if ($scope.taxHierarchieSelected.phylum) $scope.taxHierarchieSelectedPH = {'phylum': $scope.taxHierarchieSelected.phylum, 'nb_tx_ph': $scope.taxHierarchieSelected.nb_tx_ph };
+
+          if ($scope.taxHierarchieSelected.phylum)
+            $scope.taxHierarchieSelectedPH = {'phylum': $scope.taxHierarchieSelected.phylum, 'nb_tx_ph': $scope.taxHierarchieSelected.nb_tx_ph };
           else $scope.taxHierarchieSelectedPH = undefined;
-          if ($scope.taxHierarchieSelected.classe) $scope.taxHierarchieSelectedCL = {'classe': $scope.taxHierarchieSelected.classe, 'nb_tx_cl': $scope.taxHierarchieSelected.nb_tx_cl };
+
+          if ($scope.taxHierarchieSelected.classe)
+            $scope.taxHierarchieSelectedCL = {'classe': $scope.taxHierarchieSelected.classe, 'nb_tx_cl': $scope.taxHierarchieSelected.nb_tx_cl };
           else $scope.taxHierarchieSelectedCL = undefined;
-          if ($scope.taxHierarchieSelected.ordre) $scope.taxHierarchieSelectedOR = {'ordre': $scope.taxHierarchieSelected.ordre, 'nb_tx_or': $scope.taxHierarchieSelected.nb_tx_or };
+
+          if ($scope.taxHierarchieSelected.ordre)
+            $scope.taxHierarchieSelectedOR = {'ordre': $scope.taxHierarchieSelected.ordre, 'nb_tx_or': $scope.taxHierarchieSelected.nb_tx_or };
           else $scope.taxHierarchieSelectedOR = undefined;
-          if ($scope.taxHierarchieSelected.famille) $scope.taxHierarchieSelectedFM = {'famille': $scope.taxHierarchieSelected.famille, 'nb_tx_fm': $scope.taxHierarchieSelected.nb_tx_fm };
+
+          if ($scope.taxHierarchieSelected.famille)
+            $scope.taxHierarchieSelectedFM = {'famille': $scope.taxHierarchieSelected.famille, 'nb_tx_fm': $scope.taxHierarchieSelected.nb_tx_fm };
           else $scope.taxHierarchieSelectedFM = undefined;
+
+          if ($scope.taxHierarchieSelected.sousfamille)
+            $scope.taxHierarchieSelectedSFM = {'sousfamille': $scope.taxHierarchieSelected.sousfamille, 'nb_tx_sfm': $scope.taxHierarchieSelected.nb_tx_sfm };
+          else $scope.taxHierarchieSelectedSFM = undefined;
+
+          if ($scope.taxHierarchieSelected.tribu)
+            $scope.taxHierarchieSelectedTR = {'tribu': $scope.taxHierarchieSelected.tribu, 'nb_tx_tr': $scope.taxHierarchieSelected.nb_tx_tr };
+          else $scope.taxHierarchieSelectedTR = undefined;
         }
       }, true);
 
@@ -33,10 +50,17 @@ app.directive('addHierarchieDir', ['$http', 'backendCfg', function ($http,backen
       $scope.getTaxonHierarchie = function(rang, val, model) {
         var queryparam = {params : {'ilike': val.trim()}} ;
         if (model) {
-            if ((model.regne) && (rang !=='KD')) queryparam.params.regne = model.regne.trim();
-            if ((model.phylum) && ((rang !=='PH') && (rang !=='KD')))  queryparam.params.phylum = model.phylum.trim();
-            if ((model.classe) && ((rang !=='CL') && (rang !=='PH') && (rang !=='KD'))) queryparam.params.classe = model.classe.trim();
-            if ((model.ordre) && ((rang !=='OR') && (rang !=='CL') && (rang !=='PH') && (rang !=='KD'))) queryparam.params.ordre = model.ordre.trim();
+            if ((model.regne) && (rang !=='KD'))
+              queryparam.params.regne = model.regne.trim();
+
+            if ((model.phylum) && ((rang !=='PH') && (rang !=='KD')))
+              queryparam.params.phylum = model.phylum.trim();
+
+            if ((model.classe) && ((rang !=='CL') && (rang !=='PH') && (rang !=='KD')))
+              queryparam.params.classe = model.classe.trim();
+
+            if ((model.ordre) && ((rang !=='OR') && (rang !=='CL') && (rang !=='PH') && (rang !=='KD')))
+              queryparam.params.ordre = model.ordre.trim();
         }
         return $http.get(backendCfg.api_url+this.searchUrl+rang, queryparam).then(function(response){
           return response.data;
